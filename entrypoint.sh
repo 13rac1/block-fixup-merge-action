27902d4aa3383d23be2ba8cafe50c49bdeaed74a
Brad Erickson <bderickson@gmail.com>
2020-01-21 15:57:54 -0800

    fix: Fail when git command fails
    The || true was hiding the git command failure and passing the Action.
    
    For #6

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ main() {
   # Using git directly because the $GITHUB_EVENT_PATH file only shows commits in
   # most recent push.
 
-  FIXUP_COUNT=`git log --pretty=format:%s origin/master..origin/${GITHUB_REF:11} | grep fixup! | wc -l || true`
+  # Get the list before the "|| true" to fail the script when the git cmd fails.
+  FIXUP_LIST=`git log --pretty=format:%s origin/master..origin/${GITHUB_REF:11}`
+  FIXUP_COUNT=`echo $FIXUP_LIST | grep fixup! | wc -l || true`
   echo "Fixup! commits: ${FIXUP_COUNT}"
   if [ "$FIXUP_COUNT" -gt "0" ]; then
     git log --pretty=format:%s origin/master..origin/${GITHUB_REF:11} | grep fixup!
