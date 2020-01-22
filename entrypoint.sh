cea0a6486dd6b56758d76016377e1c6ef113dcbc
Brad Erickson <bderickson@gmail.com>
2020-01-21 17:36:16 -0800

    fix: Abort on master branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,15 @@ main() {
   BRANCH=${GITHUB_REF:11}
   echo "Current branch: ${BRANCH}"
 
+  if [ "$BRANCH" == "master" ]; then
+    echo "No check of master branch needed."
+    exit 0
+  fi
+
   # Using git directly because the $GITHUB_EVENT_PATH file only shows commits in
   # most recent push.
   /usr/bin/git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin master
-  if [ "$BRANCH" != "master" ]; then
-    /usr/bin/git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --shallow-exclude=master origin ${BRANCH}
-  fi
-
+  /usr/bin/git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --shallow-exclude=master origin ${BRANCH}
 
   # Get the list before the "|| true" to fail the script when the git cmd fails.
   FIXUP_LIST=`/usr/bin/git log --pretty=format:%s origin/master..origin/${BRANCH}`
